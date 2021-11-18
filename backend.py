1e6c828a7da0fd31ef4093a75ccf80149e3630e9
kush <kaushal95300@gmail.com>
2021-11-19 03:10:21 +0530

    main function added

--- backend.py
+++ backend.py
@@ -226,12 +226,12 @@ def main():
 
     """
     project=input("Enter your website name :")
-    #print("Your website name is :",project)
-    #create_key(project+"_key_pair")
-    #sg_id=create_sg(sgname=project+"_sg",save_file=project+"_detail.txt",save_file_mode="a",save=True,description="Sg_for_"+project)
-    #print("Security Group ID:",sg_id)
-    #create_sg_rule(sg_id,80,"tcp")
-    #create_sg_rule(sg_id,22,"tcp")
+    print("Your website name is :",project)
+    create_key(project+"_key_pair")
+    sg_id=create_sg(sgname=project+"_sg",save_file=project+"_detail.txt",save_file_mode="a",save=True,description="Sg_for_"+project)
+    print("Security Group ID:",sg_id)
+    create_sg_rule(sg_id,80,"tcp")
+    create_sg_rule(sg_id,22,"tcp")
     instance_id=create_instance(keyname=project+"_key_pair",sgname=project+"_sg",save_file=project+"_detail.txt",save_file_mode="a",save=True,instance_type="t2.micro",image_id="ami-0f1fb91a596abf28d")
     print("Instance ID is :",instance_id)
 
